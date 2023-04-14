3e41269602cb77a5ec2940667e601c6acddb207c
horizon.eli07@gmail.com <E2&p&JX4yIH2q0DEn322mH$9IK7Kjgog>
2023-04-14 16:13:36 -0700

    Add function interfaces

--- sudoku_checker.py
+++ sudoku_checker.py
@@ -1,5 +1,42 @@
 # https://leetcode.com/problems/valid-sudoku/
+"""
+Strategy:
+First break down board into regions
+Regions may be rows, columns, or sub-boxes
+Next, check if each region is valid
+If all regions are valid, board is valid
+"""
 class Solution(object):
+
+    def isValidRegion(self, region):
+        """
+        Takes a region and checks if it is valid
+        :type region: List[str]
+        :rtype: bool
+        """
+        return False
+    
+    def getRows(self, board):
+        """
+        Takes a board and returns a list of rows
+        :type board: List[List[str]]
+        :rtype: List[List[str]]
+        """
+    
+    def getCols(self, board):
+        """
+        Takes a board and returns a list of columns
+        :type board: List[List[str]]
+        :rtype: List[List[str]]
+        """
+    
+    def getSubBoxes(self, board):
+        """
+        Takes a board and returns a list of columns
+        :type board: List[List[str]]
+        :rtype: List[List[str]]
+        """
+
     def isValidSudoku(self, board):
         """
         :type board: List[List[str]]
